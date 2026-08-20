b89d8c6a7b09910e59667497b15cd1d17507efe4
MagicCode-ai <contact@magiccode-ai.com>
2026-08-20 16:11:57 +0800

    Release v1.2.1

--- interface/mc_interface.h
+++ interface/mc_interface.h
@@ -19,8 +19,8 @@
  *              initialization, image processing, parameter control, resource release,
  *              and version query.
  * @author      MagicCode  Team
- * @date        2026-08-18
- * @version     V1.2.0
+ * @date        2026-08-20
+ * @version     V1.2.1
  * @copyright   Copyright (C) 2024-2026 MagicCode Technology Co., Ltd. All rights reserved.
  * @website     https://www.magiccode-ai.com
  ************************************************************************************/
